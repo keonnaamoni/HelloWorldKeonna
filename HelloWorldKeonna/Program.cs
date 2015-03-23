4449a75554589a13fee47e965435550c4ea22ff4
keonnaamoni <keonnaevans@gmail.com>
2015-03-23 13:44:04 -0400

    Added first and last name
    added first and last name

---
+++ HelloWorldKeonna/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldKeonna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Keonna";
+
+           // Console.WriteLine("Hello"  + myName);  //added the Name Keonna
+            //Console.ReadLine();
+
+            string firstName = "Keonna";  //added first name
+            string lastName = "Evans";    //added last name
+
+            Console.WriteLine("Hello " + firstName + lastName);
+            Console.ReadLine();
+
+
+        }
+    }
+}
